360ad9c6478fd6d43e8237aac12c23e75d53f4a0
Mark Daniel Edillor <edillormark2@gmail.com>
2024-01-13 06:25:33 +0800

    importing express

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
 import helmet from "helmet";
+import express from "express";
 
 dotenv.config();
 
