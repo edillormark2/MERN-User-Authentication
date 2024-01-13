ddac152dc1e991c7b5dce6594fddafc413f31b3a
Mark Daniel Edillor <edillormark2@gmail.com>
2024-01-14 02:04:21 +0800

    modified route and footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () =>
-  <footer className="dark:text-gray-200 text-gray-700 text-center text-xs md:text-sm absolute bottom-4 w-full">
+  <footer className="dark:bg-main-dark-bg dark:text-gray-200 text-gray-700 text-center text-xs md:text-sm w-full p-4">
     <p>© 2024 All rights reserved by Mark Daniel Edillor</p>
   </footer>;
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,11 +5,11 @@ import { FiSun, FiMoon } from "react-icons/fi";
 import { useStateContext } from "../redux/ContextProvider";
 
 const Header = () => {
-  const { currentUser } = useSelector(state => state.user);
+  const { currentUser } = useSelector((state) => state.user);
   const [menuClicked, setMenuClicked] = useState(""); // State for active menu
   const { setMode, currentMode } = useStateContext();
 
-  const handleMenuClick = menu => {
+  const handleMenuClick = (menu) => {
     setMenuClicked(menu);
   };
 
@@ -19,9 +19,9 @@ const Header = () => {
 
   return (
     <div
-      className={`bg-white drop-shadow-xl ${currentMode === "Dark"
-        ? "dark:bg-secondary-dark-bg dark:text-gray-200"
-        : ""}`}
+      className={`bg-white drop-shadow-xl ${
+        currentMode === "Dark" ? "dark:bg-secondary-dark-bg dark:text-gray-200" : ""
+      }`}
     >
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3 ">
         <h1 className="font-bold text-sm sm:text-sm xl:text-lg hidden md:block">
@@ -29,58 +29,60 @@ const Header = () => {
         </h1>
 
         <ul className="flex gap-4 items-center ml-auto">
-          <Link to="/" onClick={() => handleMenuClick("home")}>
-            <li
-              className={`dark:hover:text-black ${menuClicked === "home"
-                ? "bg-gray-300 dark:text-gray-800"
-                : "hover:bg-gray-200"} p-2 sm:p-3 rounded-full text-sm sm:text-base`}
-            >
-              Home
-            </li>
-          </Link>
-          <Link to="/about" onClick={() => handleMenuClick("about")}>
-            <li
-              className={`dark:hover:text-black ${menuClicked === "about"
-                ? "bg-gray-300 dark:text-gray-800"
-                : "hover:bg-gray-200"} p-2 sm:p-3 rounded-full text-sm sm:text-base`}
-            >
-              About
-            </li>
-          </Link>
-          <Link to="/profile">
-            {currentUser
-              ? <div
-                  className={`flex items-center dark:hover:text-black ${menuClicked ===
-                  "profile"
-                    ? "bg-gray-300 dark:text-gray-800"
-                    : "hover:bg-gray-200"} p-1 sm:p-2 rounded-full text-sm sm:text-base`}
-                  onClick={() => handleMenuClick("profile")}
-                >
-                  <img
-                    src={currentUser.profilePicture}
-                    alt="profile"
-                    className="h-8 w-8 rounded-full object-cover"
-                  />
-                  <p className="ml-2 hidden md:block">
-                    {currentUser.username}
-                  </p>
-                </div>
-              : <li
-                  className={`dark:hover:text-black ${menuClicked === "signin"
+          {currentUser && (
+            <>
+              <Link to="/" onClick={() => handleMenuClick("home")}>
+                <li
+                  className={`dark:hover:text-black ${menuClicked === "home"
                     ? "bg-gray-300 dark:text-gray-800"
                     : "hover:bg-gray-200"} p-2 sm:p-3 rounded-full text-sm sm:text-base`}
-                  onClick={() => handleMenuClick("signin")}
                 >
-                  Sign In
-                </li>}
+                  Home
+                </li>
+              </Link>
+              <Link to="/about" onClick={() => handleMenuClick("about")}>
+                <li
+                  className={`dark:hover:text-black ${menuClicked === "about"
+                    ? "bg-gray-300 dark:text-gray-800"
+                    : "hover:bg-gray-200"} p-2 sm:p-3 rounded-full text-sm sm:text-base`}
+                >
+                  About
+                </li>
+              </Link>
+            </>
+          )}
+
+          <Link to="/profile">
+            {currentUser ? (
+              <div
+                className={`flex items-center dark:hover:text-black ${menuClicked === "profile"
+                  ? "bg-gray-300 dark:text-gray-800"
+                  : "hover:bg-gray-200"} p-1 sm:p-2 rounded-full text-sm sm:text-base`}
+                onClick={() => handleMenuClick("profile")}
+              >
+                <img
+                  src={currentUser.profilePicture}
+                  alt="profile"
+                  className="h-8 w-8 rounded-full object-cover"
+                />
+                <p className="ml-2 hidden md:block">{currentUser.username}</p>
+              </div>
+            ) : (
+              <li
+                className={`dark:hover:text-black ${menuClicked === "signin"
+                  ? "bg-gray-300 dark:text-gray-800"
+                  : "hover:bg-gray-200"} p-2 sm:p-3 rounded-full text-sm sm:text-base`}
+                onClick={() => handleMenuClick("signin")}
+              >
+                Sign In
+              </li>
+            )}
           </Link>
           <div
             className={`flex items-center cursor-pointer bg-gray-200 p-2 rounded-xl dark:text-black`}
             onClick={toggleTheme}
           >
-            {currentMode === "Dark"
-              ? <FiMoon size={20} />
-              : <FiSun size={20} />}
+            {currentMode === "Dark" ? <FiMoon size={20} /> : <FiSun size={20} />}
           </div>
         </ul>
       </div>
