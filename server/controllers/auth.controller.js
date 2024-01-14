2effcc5ca58a7138bae380ee32353088ca58b059
Mark Daniel Edillor <edillormark2@gmail.com>
2024-01-15 06:39:23 +0800

    syntax error fixed

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/user.model.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
